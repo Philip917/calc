0fc7b27801484ec7e7793919fb8820f0504673cf
Philip Koekemoer <philips.progdev@gmail.com>
2019-10-16 14:30:54 +0200

    Updates

---
+++ precache-manifest.698171fdcc87964d613ec41a23bde18f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afc5a560f25442131cd5f2de6566c7a",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "3cda3843f7aff8bb5908",
+    "url": "/calc/static/css/main.1f927835.chunk.css"
+  },
+  {
+    "revision": "57e015b3321dd18176bd",
+    "url": "/calc/static/js/2.19883d82.chunk.js"
+  },
+  {
+    "revision": "3cda3843f7aff8bb5908",
+    "url": "/calc/static/js/main.8c5d8d6a.chunk.js"
+  },
+  {
+    "revision": "e726e60f8044a7170270",
+    "url": "/calc/static/js/runtime-main.be333ecb.js"
+  }
+]);
